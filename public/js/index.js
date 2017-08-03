15e771216f7f47526a505db42e2ad4e87b27bbf4
Evan <e.godon@outlook.com>
2017-08-03 18:56:10 -0400

    Add form for messages

--- public/js/index.js
+++ public/js/index.js
@@ -11,5 +11,20 @@ socket.on('disconnect', () => {
 
 
 socket.on('newMessage', (message) => {
-    console.log(message)
+    console.log(message);
+    const li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    $('#messages').append(li);
 });
+
+$('#message-form').on('submit', (e) => {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, () => {
+
+    })
+} )
