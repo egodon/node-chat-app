c8edea6eac8d8fa59fc0230f40c3aa63d8b1daa3
Evan <e.godon@outlook.com>
2017-08-05 12:13:34 -0400

    Add css

--- public/js/index.js
+++ public/js/index.js
@@ -30,26 +30,31 @@ socket.on('newLocationMessage', (message) => {
 
 $('#message-form').on('submit', (e) => {
     e.preventDefault();
-
+     const $messageTextbox = $('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: $messageTextbox.val()
     }, () => {
+        $messageTextbox.val('');
     });
 });
 
-const locationButton = $('#send-location');
-locationButton.on('click', () => {
+const $locationButton = $('#send-location');
+$locationButton.on('click', () => {
    if(!navigator.geolocation){
        return alert('Geolocation not supported by your browser.')
    }
 
+   $locationButton.attr('disabled', 'disabled').text('Sending location...');
+
    navigator.geolocation.getCurrentPosition((position) => {
+       $locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longiture: position.coords.longitude
         })
    }, () => {
+       $locationButton.removeAttr('disabled').text('Send location');
        alert('Unable to fetch location.')
    })
 });
