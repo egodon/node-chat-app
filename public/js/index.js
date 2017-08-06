a5db7f505962b32eda5afd0bf7a12a7b6f0da5e8
Evan <e.godon@outlook.com>
2017-08-05 20:25:43 -0400

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 const socket = io();
 
+function scrollToBottom() {
+    // Selectors
+    const $messages = $('#messages');
+    const newMessage = $messages.children('li:last-child');
+    // Heights
+    const clientHeight = $messages.prop('clientHeight');
+    const scrollTop = $messages.prop('scrollTop');
+    const scrollHeight = $messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        $messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', () => {
     console.log('Connected to server');
 
@@ -20,6 +36,7 @@ socket.on('newMessage', (message) => {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 
 });
 
@@ -33,6 +50,7 @@ socket.on('newLocationMessage', (message) => {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 $('#message-form').on('submit', (e) => {
